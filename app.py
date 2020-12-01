568d60ca6c564d5120f60309c2b0f1dcd858ed13
sophie-apples <sophieannemurray@gmail.com>
2020-12-01 13:04:05 +0000

    register form submits to db, flashed messages displayed if username exists or if registration complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import(
+from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
@@ -27,6 +27,24 @@ def get_lessons():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put new user into session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
